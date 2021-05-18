03b5f4f0c4c1a7a77ed22948bc0baec71bd4014f
Shathin <shathin.rao@gmail.com>
2021-05-18 20:25:06 +0530

    🔨 minor theme changes

--- lib/utils/theme/themes/custom_text_theme.dart
+++ lib/utils/theme/themes/custom_text_theme.dart
@@ -4,67 +4,67 @@ import 'package:flutter/material.dart';
 /// This is based on Flutter's [ThemeData] class
 class CustomTextTheme {
   static final TextStyle headline1 = TextStyle(
-    fontSize: 96.0,
+    fontSize: 54.0,
     fontWeight: FontWeight.w300,
     letterSpacing: -1.5,
   );
   static final TextStyle headline2 = TextStyle(
-    fontSize: 60.0,
+    fontSize: 48.0,
     fontWeight: FontWeight.w300,
     letterSpacing: -0.5,
   );
   static final TextStyle headline3 = TextStyle(
-    fontSize: 48.0,
+    fontSize: 36.0,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.0,
   );
   static final TextStyle headline4 = TextStyle(
-    fontSize: 34.0,
+    fontSize: 26.0,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.25,
   );
   static final TextStyle headline5 = TextStyle(
-    fontSize: 24.0,
+    fontSize: 20.0,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.0,
   );
   static final TextStyle headline6 = TextStyle(
-    fontSize: 20.0,
+    fontSize: 16.0,
     fontWeight: FontWeight.w500,
     letterSpacing: 0.15,
   );
   static final TextStyle subtitle1 = TextStyle(
-    fontSize: 16.0,
+    fontSize: 14.0,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.15,
   );
   static final TextStyle subtitle2 = TextStyle(
-    fontSize: 14.0,
+    fontSize: 12.0,
     fontWeight: FontWeight.w500,
     letterSpacing: 0.1,
   );
   static final TextStyle bodyText1 = TextStyle(
-    fontSize: 16.0,
+    fontSize: 14.0,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.5,
   );
   static final TextStyle bodyText2 = TextStyle(
-    fontSize: 14.0,
+    fontSize: 12.0,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.25,
   );
   static final TextStyle button = TextStyle(
-    fontSize: 14.0,
+    fontSize: 12.0,
     fontWeight: FontWeight.w500,
     letterSpacing: 1.25,
   );
   static final TextStyle caption = TextStyle(
-    fontSize: 12.0,
+    fontSize: 10.0,
     fontWeight: FontWeight.w400,
     letterSpacing: 0.4,
   );
   static final TextStyle overline = TextStyle(
-    fontSize: 10.0,
+    fontSize: 8.0,
     fontWeight: FontWeight.w400,
     letterSpacing: 1.5,
   );
